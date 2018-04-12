d804a9ee57fa0b6a5ab48c5fa5ef0ec8534dbeb2
chioreandan <chiorean.dan12@gmail.com>
2018-04-12 22:43:50 +0300

    SQL Query Updated

--- ShopV04/ShopV04/Controllers/HomeController.cs
+++ ShopV04/ShopV04/Controllers/HomeController.cs
@@ -14,15 +14,15 @@ namespace ShopV04.Controllers
         }
 
         public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+         {
+             ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+             return View();
+         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Users = "Your contact page.";
 
             return View();
         }
--- ShopV04/ShopV04/Controllers/UserController.cs
+++ ShopV04/ShopV04/Controllers/UserController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShopV04;
 
 namespace ShopV04.Controllers
 {
     public class UserController : Controller
     {
+        shopEntities db = new shopEntities();
         // GET: User
-        public ActionResult Index()
+        public ActionResult Users()
         {
+            ViewBag.Message = db.users.ToList();
             return View();
         }
     }
